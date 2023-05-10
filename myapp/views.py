4a55e02e57a30f68b19257a1725c1bdc87ba0308
Theboque <dedgar2335@gmail.com>
2023-05-09 20:16:42 -0600

    Se agrega la view indexpage la cual se mostrará como pagína principal
    Se modifica la url <hello> en donde ahora recibe un parametro <username> de tipo str.
    Es con el fin de que al momento de ingresar a la view <hello> esta necesite dicho parametro para mostrar el contenido
    el cual solo será una concatenación de la palabra hello con el parámetro.

--- myapp/views.py
+++ myapp/views.py
@@ -3,9 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def index(request):
+    return HttpResponse("<h2>index page </h2>")
+    
 
-def hello(request):
-    return HttpResponse("<h1>Hello word</h1>")
+def hello(request, username):
+    print(username)
+    return HttpResponse("<h1>Hello %s</h1>" %username)
 
 def about(request):
     return HttpResponse("<h2>About </h2>")
